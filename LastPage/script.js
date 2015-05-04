37e9c5c45c156a7456de5661f8d07ee588f87efb
lyyvalhalla <lyy.yiyang@gmail.com>
2015-05-03 21:27:14 -0400

    keywords each year ready

--- LastPage/script.js
+++ LastPage/script.js
@@ -1,7 +1,7 @@
 var keyWordArray =[], timeStampArray =[];
 var delta;
 var colors = ["#222222", "#193969", "#1A2A45", "#A62D2A","#FFB600", "#222222" ];
-
+var wordsArray = [];
 function setupKeyWord() {
 
     var years =[];
@@ -13,8 +13,6 @@ function setupKeyWord() {
 
 
     var bodyWidth = document.body.style.width = 1398*(years.length +1);
-    console.log(bodyWidth);
-    // document.body.style.background = "#222222";
     var keyBar = document.getElementById('key-bar');
     keyBar.style.left = "1398";
     
@@ -31,8 +29,16 @@ function setupKeyWord() {
         timeTag.style.background = "#ffffff";
         keyBar.appendChild(timeTag);
         // console.log(timeTag.style.left);
+
+        for (var j=0; j<keyWord[years[i]].length; j++) {
+            // console.log(keyWord[years[i]][j].word);
+            // var words = document.getElementById('title');
+            // words.textContent =  keyWord[years[i]][j].word;
+        }
     }
 
+
+    var backgrounds =[];
     for (var i=0; i< (years.length + 1); i++) {
         var backGround = document.createElement('div');
         backGround.className = "back-ground";
@@ -42,8 +48,40 @@ function setupKeyWord() {
         backGround.style.left = i*1398;
         backGround.style.width = 1398;
         document.body.appendChild(backGround);
+        backgrounds.push(backGround);
     }
-    
+
+    var eachArray=[];
+
+    $(window).scroll(function() {
+        
+        for (var i=0; i<years.length; i++) {
+            var wordsArray =[];
+            for (var j=0; j<keyWord[years[i]].length; j++) {
+                // console.log(keyWord[years[i]][j].word)
+                wordsArray.push(keyWord[years[i]][j].word);;
+            }
+            
+            eachArray.push(wordsArray);
+            var words = document.getElementById('title');
+            words.style.display = "inline-block";
+            words.style.width = 10;
+            words.style.height = 100;
+            // console.log(((parseInt(backgrounds[i+1].style.left))+1398) + "; " + parseInt(backgrounds[i+1].style.left));
+            if (window.pageXOffset < (parseInt(backgrounds[i+1].style.left)+1398) && window.pageXOffset > parseInt(backgrounds[i+1].style.left)) {
+                console.log("woof");
+                words.textContent = eachArray[i];
+                // console.log(eachArray[i]);
+            }
+
+            if (window.pageXOffset < 1398) {
+                words.textContent = "bookmark bio";
+            }
+        }
+
+        
+    });
+
 }
 
 
